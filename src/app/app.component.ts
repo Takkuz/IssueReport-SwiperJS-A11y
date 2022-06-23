908306208da08de4a6c3165de896d29b372a38a0
Vittorio Giovanelli <vittorio.giovanelli@euris.it>
2022-06-23 11:31:23 +0200

    Adding SwiperJS and instructions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import SwiperCore, { A11y, Navigation, Pagination, SwiperOptions } from 'swiper';
+
+SwiperCore.use([
+  Navigation,
+  Pagination,
+  A11y
+])
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SwiperJS-Angular';
+  swiperConfig: SwiperOptions = {
+    a11y: { enabled: true },
+    pagination: true
+  }
 }
